6586bc0d737d11c4b18abefa9dcf5df804782e24
esyanova7 <esyanova7@gmail.com>
2022-06-13 18:37:46 +0300

    firstcommit

---
+++ Task1.java
@@ -0,0 +1,21 @@
+package task.pack;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        int[] array = {11,44,63,7,17,31,101,3,144,10};
+        int max=array[0];
+        int min=array[0];
+        for (int check:array) {
+            if(check > max) {
+                max=check;
+            }else if (check < min) {
+                min=check;
+            }
+        }
+        System.out.println(max);
+
+        System.out.println(min);
+    }
+
+}
